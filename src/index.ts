d6b57dcac700d09023f10d64acba22e221067c77
weerachit utnok <weerachit.utnok01@gmail.com>
2024-08-06 15:51:59 +0700

    fix: code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 
-const app = new Hono()
+const app = new Hono().basePath('/api')
 
 app.use(cors({ origin: '*' })) // กำหนด origin ที่คุณต้องการอนุญาต
 
